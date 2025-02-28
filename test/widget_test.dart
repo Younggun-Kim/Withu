cf757468280cb10bddd10ac0eb48df332730fb8a
Conner <dudrjs145@naver.com>
2025-02-28 17:45:28 +0900

    remove: 필요없는 코드 제거

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,7 @@
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_core_platform_interface/test.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart' show Mock;
-
-class MockFirebaseCore extends Mock implements Firebase {}
-
-class MockFirebaseAnalytics extends Mock implements FirebaseAnalytics {}
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {});
-}
-
-void setUpFirebaseAnalytics() {
-  setupFirebaseCoreMocks();
 }
